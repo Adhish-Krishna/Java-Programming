b4481bdd4a26eee5185a3794215598c393ee1840
Adhish-Krishna <adhishthesak@gmail.com>
2024-09-27 16:32:24 +0530

    Add Electricity.java

---
+++ OOPSLab/PracticeSheet2/EelectricityBill.java
@@ -0,0 +1,27 @@
+package OOPSLab.PracticeSheet2;
+
+import java.util.Scanner;
+
+class Bill{
+
+}
+
+class Persons extends Bill{
+  
+}
+
+
+
+public class EelectricityBill {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    try{
+
+    }
+    finally{
+      if(scan!=null){
+        scan.close();
+      }
+    }
+  }
+}
